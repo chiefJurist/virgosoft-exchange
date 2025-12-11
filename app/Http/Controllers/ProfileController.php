1515e897c659beb6e23f004eada8afbbefb04030
chiefJurist <anthonynnanna004@gmail.com>
2025-12-11 21:43:50 +0100

    profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $user = $request->user()->load('assets');
+
+        return response()->json([
+            'balance' => $user->balance,
+            'assets'  => $user->assets->keyBy('symbol')->map(fn($a) => [
+                'amount'        => $a->amount,
+                'locked_amount' => $a->locked_amount,
+                'available'     => $a->amount - $a->locked_amount,
+            ]),
+        ]);
+    }
 }
